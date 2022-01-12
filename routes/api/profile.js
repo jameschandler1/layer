d5459be0109cad6cbf4a2d3270235aea9d2ed63b
James Chandler <stillsound@protonmail.com>
2022-01-12 14:31:44 -0800

    Delete route to delete user and profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -127,6 +127,28 @@ router.get("/user/:user_id", async (req, res) => {
     res.json(profile);
   } catch (err) {
     console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
+//@route    DELETE api/profile
+//@desc     delete profile, user and posts
+//@access  Private
+
+router.delete("/", auth, async (req, res) => {
+  try {
+    //@todo - remove users posts
+    //remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    await User.findOneAndRemove({_id: req.user.id });
+
+    res.json({msg: "User deleted successfully"});
+
+  } catch (err) {
+    console.error(err.message);
     res.status(500).send("Server Error");
   }
 });
