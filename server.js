cd74282e9b14821495885b1b0e714d566bfe9b65
James Chandler <stillsound@protonmail.com>
2022-01-11 20:55:53 -0800

    create routes and config folder, add relevant files configure db

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const connectdb = require('./config/db');
 
 const app = express();
 
+//connect database
+connectdb();
+
 app.get('/', (req, res) => res.send(`API is running`));
-
-
 
 PORT = process.env.PORT || 5000;
 
