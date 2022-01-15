b848fb816593bb4ffddd8ab43904c42402268628
James Chandler <stillsound@protonmail.com>
2022-01-14 16:57:59 -0800

    update post route

--- models/Post.js
+++ models/Post.js
@@ -2,54 +2,62 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
-    user: {
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
         type: Schema.Types.ObjectId,
-        ref: 'users'
+        ref: "users",
+      },
     },
-    text: {
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+      },
+      text: {
         type: String,
         required: true,
-    },
-    name: {
-        type: String
-    },
-    avatar: {
-        type: String
-    },
-    likes: [
-        {
-            user: {
-                type: Schema.Types.ObjectId,
-                ref: 'users'
-            }
-        }
-    ],
-    comments: [
-        {
-            user: {
-                type: Schema.Types.ObjectId,
-                ref: 'users'
-            },
-            text: {
-                type: String,
-                required: true,
-            },
-            name: {
-                type: String
-            },
-            avatar: {
-                type: String
-            },
-            date: {
-                type: Date,
-                default: Date.now
-            }
-        }
-    ],
-    date: {
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
         type: Date,
-        default: Date.now
-    }
+        default: Date.now,
+      },
+      likes: [
+        {
+          user: {
+            type: Schema.Types.ObjectId,
+            ref: "users",
+          },
+        },
+      ],
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = Post = mongoose.model('posts', PostSchema);
--- models/Profile.js
+++ models/Profile.js
@@ -5,9 +5,9 @@ const ProfileSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'user'
     },
-    company: {
-        type: String
-    },
+    // company: {
+    //     type: String
+    // },
     website: {
         type: String,
         required: true
@@ -19,76 +19,80 @@ const ProfileSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    skills: {
+    intrests: {
         type: [String],
         required: true
     },
+    // skills: {
+    //     type: [String],
+    //     required: true
+    // },
     bio: {
         type: String
     },
-    githubusername: {
-        type: String
-    },
-    experience: [
-        {
-            title: {
-                type: String,
-                required: true
-            },
-            company: {
-                type: String,
-                required: true
-            },
-            location: {
-                type: String
-            },
-            from: {
-                type: Date,
-                required: true
-            },
-            to: {
-                type: Date
-            },
-            current: {
-                type: Boolean,
-                default: false
-            },
-            description: {
-                type: String
-            }
-        }
-    ],
-    education: [
-        {
-            school: {
-                type: String,
-                required: true
-            },
-            degree: {
-                type: String,
-                required: true
-            },
-            fieldofstudy: {
-                type: String,
-                required: true
-            },
-            from: {
-                type: Date,
-                required: true
-            },
-            to: {
-                type: Date
-            },
-            current: {
-                type: Boolean,
-                default: false
-            },
-            description: {
-                type: String
+    // githubusername: {
+    //     type: String
+    // },
+    // experience: [
+    //     {
+    //         title: {
+    //             type: String,
+    //             required: true
+    //         },
+    //         company: {
+    //             type: String,
+    //             required: true
+    //         },
+    //         location: {
+    //             type: String
+    //         },
+    //         from: {
+    //             type: Date,
+    //             required: true
+    //         },
+    //         to: {
+    //             type: Date
+    //         },
+    //         current: {
+    //             type: Boolean,
+    //             default: false
+    //         },
+    //         description: {
+    //             type: String
+    //         }
+    //     }
+    // ],
+    // education: [
+    //     {
+    //         school: {
+    //             type: String,
+    //             required: true
+    //         },
+    //         degree: {
+    //             type: String,
+    //             required: true
+    //         },
+    //         fieldofstudy: {
+    //             type: String,
+    //             required: true
+    //         },
+    //         from: {
+    //             type: Date,
+    //             required: true
+    //         },
+    //         to: {
+    //             type: Date
+    //         },
+    //         current: {
+    //             type: Boolean,
+    //             default: false
+    //         },
+    //         description: {
+    //             type: String
 
-            }
-        }
-    ],
+    //         }
+    //     }
+    // ],
     social: {
         youtube: {
             type: String
